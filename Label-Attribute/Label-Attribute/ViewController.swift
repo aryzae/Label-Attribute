e0fe64a4f228fe3510969c50c1c3bab423fdd82a
aryzae <a.aryzae@gmail.com>
2018-09-08 04:07:07 +0900

    Implemented code

--- Label-Attribute/Label-Attribute/ViewController.swift
+++ Label-Attribute/Label-Attribute/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var label: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        let rubyView = RubyView(frame: CGRect.init(origin: .zero, size: CGSize.init(width: view.frame.width, height: 100)).insetBy(dx: 20, dy: 0))
+        view.addSubview(rubyView)
     }
 
 
+}
+// 参考　https://qiita.com/woxtu/items/284369fd2654edac2248
+private final class RubyView: UIView {
+
+    override func draw(_ rect: CGRect) {
+        // ???
+        let each = "各種"
+        let ruby = "(税込)" as CFString
+        let yen = "¥"
+        let num = "280"
+
+        var text = [.passRetained(ruby) as Unmanaged<CFString>?, .none, .none, .none]
+        let annotation = CTRubyAnnotationCreate(.auto, .auto, 0.5, &text)
+
+        //        NSAttributedString.Key
+        let attribute = NSAttributedString(string: yen, attributes: [.font : UIFont.systemFont(ofSize: 30),
+                                                                    .foregroundColor : UIColor.black,
+                                                                    kCTRubyAnnotationAttributeName as NSAttributedString.Key: annotation])
+
+        let size = attribute.boundingRect(with: rect.size, options: .usesLineFragmentOrigin, context: nil)
+
+        let context = UIGraphicsGetCurrentContext()!
+        context.setFillColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1)
+        context.addRect(rect)
+        context.fillPath()
+
+        context.translateBy(x: (rect.width - size.width) / 2, y: 200)
+        context.scaleBy(x: 1, y: -1)
+
+        let line = CTLineCreateWithAttributedString(attribute)
+        CTLineDraw(line, context)
+    }
 }
 
