e4ff1af3071065f25a66825e9e3879e33d6865d4
aryzae <a.aryzae@gmail.com>
2018-09-07 23:05:13 +0900

    added project file

---
+++ Label-Attribute/Label-Attribute/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Label-Attribute
+//
+//  Created by Aryzae on 2018/09/07.
+//  Copyright © 2018 aryzae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
